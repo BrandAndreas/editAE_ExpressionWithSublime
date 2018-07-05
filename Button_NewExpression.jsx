1254d96ed6d4329d300658319d8b45d10d513906
BrandAndreas <33638408+BrandAndreas@users.noreply.github.com>
2018-07-05 11:49:40 +0200

    Update Button_NewExpression.jsx

--- Button_NewExpression.jsx
+++ Button_NewExpression.jsx
@@ -1,10 +1,19 @@
 //@include 'functions.js';
+//@include 'ecma5.js';
+
 var proj = app.project;
 proj.save();
-var projPath = getProjPath();
 
+var selProps = comp.selectedProperties;
 
-var newText = getExpression();
-
-openSublime();
-exportTextToSublime(newText);
+selProps.forEach(function(property){
+	var path = getPropPath(property);
+	if(!checkExistingFile(path)){
+		createFile(property);		
+	}else if(confirm('There already exists an expression!\nOverwrite it?')){
+		createFile(property);
+	}else{
+		openFile(property);
+	}
+	
+})
