a148baffc751bc857e26f646b6179b43242a9750
BrandAndreas <33638408+BrandAndreas@users.noreply.github.com>
2018-07-06 16:48:38 +0200

    Update Button_NewExpression.jsx

--- Button_NewExpression.jsx
+++ Button_NewExpression.jsx
@@ -3,12 +3,13 @@
 
 var proj = app.project;
 proj.save();
-
+var comp = activeComp();
 var selProps = comp.selectedProperties;
+createExpressionDirectory()
 
 selProps.forEach(function(property){
-	var path = getPropPath(property);
-	if(!checkExistingFile(path)){
+	// var path = getPropPath(property);
+	if(!checkExistingFile(property)){
 		createFile(property);		
 	}else if(confirm('There already exists an expression!\nOverwrite it?')){
 		createFile(property);
